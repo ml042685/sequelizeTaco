e59641c8e1f9b1222193b2eea0b8e7ccc458971b
Marcos Luna <ml042685@yahoo.com>
2020-02-02 11:14:07 -0600

    better

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -17,7 +17,7 @@ router.get('/index', function(req, res) {
 	db.Taco.findAll({}).then(function(data){
 		var hbsObject = {taco: data};
 		res.render('index', hbsObject);
-	})
+	});
 	// taco.find(function(data) {
 	// 	var hbsObject = {taco: data};
 	// 	res.render('index', hbsObject);
@@ -27,24 +27,44 @@ router.get('/index', function(req, res) {
 
 
 // insertOne
-router.post('/tacos/insertOne', function(req, res) {
-	taco.insertOne(['taco_name', 'devoured'], [req.body.name, false], function() {
-		res.redirect('/index');
+// router.post('/tacos/insertOne', function(req, res) {
+// 	taco.insertOne(['taco_name', 'devoured'], [req.body.name, false], function() {
+// 		res.redirect('/index');
+// 	});
+// });
+router.post('/index/create', function(req, res) {
+	db.Taco.create({
+		taco_name: req.body.taco_name
+	}).then(function(data) {
+		res.redirect("/");
 	});
-});
+	});
 
 
 
 // updateOne
-router.put('/tacos/updateOne/:id', function(req, res) {
-	var condition = 'id = ' + req.params.id;
-	console.log('condition', condition);
+// router.put('/tacos/updateOne/:id', function(req, res) {
+// 	var condition = 'id = ' + req.params.id;
+// 	console.log('condition', condition);
 
-	taco.updateOne({devoured: req.body.devoured}, condition, function() {
-		res.redirect('/index');
+// 	taco.updateOne({devoured: req.body.devoured}, condition, function() {
+// 		res.redirect('/index');
+	router.post("/tacos/update/:id", function(req, req) {
+		console.log(req);
+		db.Taco.update(
+			{
+				devoured: true	
+			},
+			{
+				where: {
+					id: req.params.id
+				}
+			}
+		).then(function(data) {
+			console.log(data);
+			res.json("/");
+		});
 	});
-});
-
 
 // Export 
 module.exports = router;
